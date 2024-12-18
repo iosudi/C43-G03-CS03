78c863141d29f5dac9a113729daf88a4b5277caf
sudi <saudi33gg@gmail.com>
2024-12-18 21:50:01 +0200

    Question 2

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -6,11 +6,22 @@ class Program
     {
         #region Question 1
 
+        // Console.WriteLine("Enter a number: ");
+        // int number = int.Parse(Console.ReadLine()!);
+        //
+        // // Check if the number can be divided by 3 and 4
+        // string message = number % 4 == 0 && number % 3 == 0 ? "Yes" : "No";
+        // Console.WriteLine(message);
+
+        #endregion
+
+        #region Question 2
+
         Console.WriteLine("Enter a number: ");
         int number = int.Parse(Console.ReadLine()!);
-
-        // Check if the number can be divided by 3 and 4
-        string message = number % 4 == 0 && number % 3 == 0 ? "Yes" : "No";
+        
+        // Check if the number negative or positive
+        string message = number < 0  ? "Negative" : "Positive";
         Console.WriteLine(message);
 
         #endregion
